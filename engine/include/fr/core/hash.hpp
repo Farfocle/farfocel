67d93d6a3fefb734d7a26be68420724a5269c340
Kiju <dev@jakubkijek.com>
2026-04-07 22:06:49 +0200

    feat: finish work on hash functions

--- engine/include/fr/core/hash.hpp
+++ engine/include/fr/core/hash.hpp
@@ -7,17 +7,18 @@
 
 #pragma once
 
-#include "fr/core/typedefs.hpp"
 #include <bit>
 #include <concepts>
+
+#include "fr/core/typedefs.hpp"
 
 namespace fr {
 
 struct Hash {
     U64 value;
 
-    static Hash from_raw(U64 n) noexcept {
-        return Hash{.value = n};
+    static Hash from_raw(U64 v) noexcept {
+        return Hash{.value = v};
     }
 
     /// @brief High 57  bits.
@@ -53,7 +54,7 @@ template <typename T>
 concept IsHashable = impl::HasMemberHash<T> || impl::HasADLHash<T>;
 
 /// @brief splitmix64 is a fast bijective mixer - excelent to primitives.
-inline U64 splitmix_u64(U64 v) noexcept {
+inline U64 splitmix64(U64 v) noexcept {
     v ^= v >> 30;
     v *= 0xbf58476d1ce4e5b9ULL;
     v ^= v >> 27;
@@ -63,26 +64,47 @@ inline U64 splitmix_u64(U64 v) noexcept {
 }
 
 inline Hash hash(U32 v) noexcept {
-    return Hash::from_raw(splitmix_u64(v));
+    return Hash::from_raw(splitmix64(v));
 }
 
 inline Hash hash(U64 v) noexcept {
-    return Hash::from_raw(splitmix_u64(v));
+    return Hash::from_raw(splitmix64(v));
 }
 
 inline Hash hash(S32 v) noexcept {
-    return Hash::from_raw(splitmix_u64(static_cast<U64>(v)));
+    return Hash::from_raw(splitmix64(static_cast<U64>(v)));
 }
 
 inline Hash hash(S64 v) noexcept {
-    return Hash::from_raw(splitmix_u64(static_cast<U64>(v)));
+    return Hash::from_raw(splitmix64(static_cast<U64>(v)));
 }
 
 inline Hash hash(F32 v) noexcept {
-    return Hash::from_raw(splitmix_u64(std::bit_cast<U32>(v)));
+    return Hash::from_raw(splitmix64(std::bit_cast<U32>(v)));
 }
 
 inline Hash hash(F64 v) noexcept {
-    return Hash::from_raw(splitmix_u64(std::bit_cast<U64>(v)));
+    return Hash::from_raw(splitmix64(std::bit_cast<U64>(v)));
 }
+
+inline Hash hash(bool v) noexcept {
+    return Hash::from_raw(v ? 1 : 0);
+}
+
+/// @brief Wayhash64 is a modern, non-cryptgraphic, general purpose hash function.
+inline U64 wyhash64(U64 a, U64 b) noexcept {
+    __uint128_t r = static_cast<__uint128_t>(a ^ 0x9e3779b97f4a7c15ULL) *
+                    static_cast<__uint128_t>(b ^ 0xe7037ed1a0b428dbULL);
+
+    return static_cast<U64>(r >> 64) ^ static_cast<U64>(r);
+}
+
+/// @brief Combines two hash values.
+inline Hash combine_hashes(Hash a, Hash b) noexcept {
+    return Hash::from_raw(a.value ^
+                          (b.value + 0x9e3779b97f4a7c15ULL + (a.value << 6) + (a.value >> 2)));
+}
+
+/// @brief Large prime number is a good seed for hash functions.
+inline constexpr U64 HASH_SEED = 0xa0761d6478bd642f;
 } // namespace fr
