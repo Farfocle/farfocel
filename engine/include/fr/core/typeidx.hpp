9d26655f46c524ef7ba913590409083acf47d2ac
Kiju <dev@jakubkijek.com>
2026-05-12 19:50:38 +0200

    feat: make TypeIdxGen thread safe

--- engine/include/fr/core/typeidx.hpp
+++ engine/include/fr/core/typeidx.hpp
@@ -5,34 +5,32 @@
  * @brief TypeIdx is a mechanism for generating monotonic type indexes unique per translation unit.
  */
 
-#include "fr/core/typedefs.hpp"
-
 #pragma once
+
+#include <atomic>
+
+#include "fr/core/typedefs.hpp"
 
 namespace fr {
 using TypeIdx = U32;
 
 /**
  * @brief Generates monotonic type indexes unique per translation unit. Indexes are U32.
- * @warning Not thread safe.
+ * @note Thread-safe.
  */
 class TypeIdxGen {
 public:
     /**
-     * @brief Generates TypeIdx.
+     * @brief Generates TypeIdx using the global generator.
      * @return Unique monotonic type idx.
-     * @warning Not thread safe.
      */
     template <typename T>
-    static TypeIdx gen() {
-        static TypeIdx idx = gen_next();
+    static TypeIdx gen() noexcept {
+        static const TypeIdx idx = s_counter.fetch_add(1, std::memory_order_relaxed);
         return idx;
     }
 
 private:
-    static TypeIdx gen_next() {
-        static TypeIdx counter = 0;
-        return counter++;
-    }
+    inline static std::atomic<TypeIdx> s_counter{0};
 };
 } // namespace fr
