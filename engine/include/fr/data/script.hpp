50b4eb7ce5e06521f3467dc1fd6c003cec850b24
Kiju <dev@jakubkijek.com>
2026-05-27 21:20:39 +0200

    chore(data): begin on script work

---
+++ engine/include/fr/data/script.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file script.hpp
+ * @author Kiju
+ *
+ * @brief Script offers an alternative view of the data stored in the hidden ECS.
+ * @details Karol Szypula would despise that definition.
+ */
+
+namespace fr {}
